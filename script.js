546d889019995f91340d204a2b5092ed48e9725e
oswaldocervantes23 <oswaldocervantes23@icloud.com>
2023-01-17 16:27:41 -0600

    Creando archivo encriptador.html

---
+++ script.js
@@ -0,0 +1,33 @@
+function encriptar(){
+    var frase = document.getElementById ("textoCifrado").value.toLowerCase();
+
+    var textoCifrado = frase.replace(/e/img, "enter");
+    var textoCifrado = textoCifrado.replace(/o/img, "ober");
+    var textoCifrado = textoCifrado.replace(/i/img, "imes");
+    var textoCifrado = textoCifrado.replace(/a/img, "ai");
+    var textoCifrado = textoCifrado.replace(/u/img, "ufat");
+
+    document.getElementById("textoDescifrado").innerHTML = textoCifrado;
+
+    document.getElementById("btnCopiar").style.display = "show";
+    document.getElementById("btnCopiar").style.display = "inherit";
+
+    document.getElementById("imagen-contenedor").style.display = "inherit";
+    document.getElementById("imagen-contenedor").style.display = "show";
+}
+function desencriptar(){
+    var frase = document.getElementById ("textoCifrado").value.toLowerCase();
+
+    var textoCifrado = frase.replace(/enter/img, "e");
+    var textoCifrado = textoCifrado.replace(/ober/img, "o");
+    var textoCifrado = textoCifrado.replace(/imes/img, "i");
+    var textoCifrado = textoCifrado.replace(/ai/img, "a");
+    var textoCifrado = textoCifrado.replace(/ufat/img, "u");
+
+    document.getElementById("textoDescifrado").innerHTML = textoCifrado;
+}
+function copiar(){
+    var contenido = document.querySelector("#textoDescifrado");
+    contenido.select();
+    document.execCommand("copy");
+}
